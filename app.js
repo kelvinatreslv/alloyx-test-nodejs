bc41c100804960d3fdd97e806855fab2f2d86540
Kelvin <kelvin@reslv.io>
2025-04-24 15:50:35 +0800

    init

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js CodeBuild S3 Example!');
-});
+  res.send('Hello from Alloyx Node.js App!')
+})
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
-module.exports = app; // for testing
+  console.log(`App listening on port ${port}`)
+})
